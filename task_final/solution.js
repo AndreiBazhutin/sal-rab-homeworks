56365cada212023a1bdc40113fb611342b0095d2
AndreiBazhutin <bazhytin.a.v@gmail.com>
2022-02-27 20:09:30 +0300

    Пятое исправление

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,19 +34,17 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {}, goods: [], order: {}};
+    let data = {data: {client: `${name} ${phone}`, 
+    order: {
+        address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,
+        sum: sum
+        }, 
+    goods: []
+}};
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i ++) {
-        goods[i] = {title: goods[i].title , count: goods[i].count}
-        data.goods.push(goods[i]);
-    }
-    
-    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
-    data.order.sum = sum;
-
-    data.client = name + ' ' + phone;
+for (let i = 0; i < goods.length; i += 1) {
+data.data.goods.push({title: goods[i].title, count: goods[i].count});
+}
 
     let jsonData = JSON.stringify(data);
 
